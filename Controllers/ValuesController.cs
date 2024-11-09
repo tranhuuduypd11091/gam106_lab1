211cee21a9b3a110868c736d3882a13b1b78f413
tranhuuduypd11091 <12345duytran@gmail.com>
2024-11-09 15:04:59 +0700

    lab1

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            lab1 lab1 = new lab1
+            {
+                CouresName = "Back-end game Frogamming",
+                CouresCode = "gam106",
+                Name = "TranHuuDuy",
+                StudentCode = "PD11091",
+                Class = "GA19301",
+            };
+            int status = 1;
+            string message = "get data succes!";
+            var data = new {status, message, lab1};
+            return new JsonResult(data);
+
+          
+        }
+    }
+}
---
+++ Models/lab1.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class lab1
+    {
+        public string? CouresName { get; set; }
+        public string? CouresCode { get; set; }
+        public string? Name { get; set; }
+        public string? StudentCode { get; set; }
+        public string? Class { get; set; }
+    }
+}
